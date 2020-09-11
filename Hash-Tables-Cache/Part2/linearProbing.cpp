2d85be60572a43b204d6aa2663120dcab8b57b47
ghazi221b <71129908+ghazi221b@users.noreply.github.com>
2020-09-11 17:58:10 +0500

    Add files via upload

---
+++ Hash-Tables-Cache/Part2/linearProbing.cpp
@@ -0,0 +1,122 @@
+#ifndef LINEARPROBING_CPP
+#define LINEARPROBING_CPP
+
+#include "linearProbing.h"
+#include "hashfunctions.cpp"
+
+
+HashL::HashL(){
+     tableSize = 1000; // you cant change this
+    hashTable= new block*[tableSize];
+    for(long i=0;i<tableSize;i++)
+    {
+    	hashTable[i]=NULL;
+    }
+    count=0;
+
+}
+
+HashL::~HashL(){
+    for (int i = 0 ; i < tableSize ; i++)
+      {
+            delete hashTable[i] ;
+      }
+    hashTable = NULL;
+
+}
+
+unsigned long HashL :: hash(string value){
+	  return madCompression(bitHash(value),tableSize);
+}
+
+void HashL::resizeTable(){
+	 int x=tableSize;
+	tableSize=tableSize*7;
+	block**hashTable1=new block*[tableSize];
+	for(int i=0;i<tableSize;i++)
+	{
+    	hashTable1[i]=NULL;
+	}
+	block**keep=hashTable;
+
+	for(int i=0;i<x;i++)
+	{
+    if(hashTable[i]!=NULL)
+    {
+
+     int index=hash(hashTable[i]->value);
+
+    while(hashTable1[index]!=NULL)
+    {
+        index=(index+1)%(tableSize);
+	}
+
+		hashTable1[index]=new block(index,hashTable[i]->value);
+
+    }
+
+}
+
+hashTable=hashTable1;
+delete keep;
+
+keep=NULL;
+}
+
+void HashL::insert(string value){
+	
+  unsigned long index=hash(value);
+	if(hashTable[index]!=NULL)
+	{
+		while(hashTable[index]!=NULL)
+		{
+			index =(index+1)%tableSize;
+		}
+
+		hashTable[index]=new block(index,value);
+		count++;
+
+	}
+	else
+	{
+		hashTable[index]=new block(index,value);
+		count++;
+	}
+	if(count*1.0/tableSize>=0.4)
+	{
+		resizeTable();
+	}
+
+}
+
+void HashL::deleteWord(string value){
+
+	 block*temp=lookup(value);
+   if(temp!=NULL)
+   {
+       hashTable[temp->key]=NULL;
+       delete temp;
+   }
+}
+block* HashL::lookup(string value){
+unsigned long index=hash(value);
+	if(hashTable[index]==NULL)
+	{
+		return hashTable[index];
+	}
+	else
+	{
+		while(hashTable[index]->value!=value)
+		{
+			index=(index+1)%tableSize;
+			if(hashTable[index]==NULL)
+			{
+				return NULL;
+			}
+
+		}
+		return hashTable[index];
+
+	}
+}
+#endif
